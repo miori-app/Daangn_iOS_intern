f90627e092f9c9003654626aebf8be9c28ac7846
iammiori <mioriii@naver.com>
2021-07-15 20:30:51 +0900

    [init] init the project

---
+++ DaangnClone/DaangnClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaangnClone
+//
+//  Created by miori Lee on 2021/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
