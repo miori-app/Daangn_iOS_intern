9419b02d04a6395a01088ea54eb2c7502f444577
iammiori <mioriii@naver.com>
2021-07-25 20:56:58 +0900

    [feat] viewcontroller extension(키보드터치액션)

---
+++ DaangnClone/Configuration/Extension/UIViewController.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController.swift
+//  DaangnClone
+//
+//  Created by miori Lee on 2021/07/18.
+//
+
+import UIKit
+import SnapKit
+
+extension UIViewController {
+
+    // MARK: 빈 화면을 눌렀을 때 키보드가 내려가도록 처리
+    func dismissKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer =
+            UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+//        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(false)
+    }
+
+}
+
+
